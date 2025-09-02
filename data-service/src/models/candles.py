af6688391c6492173dc01f46b097a19fbe114e28
mgordon34 <coltfanman@gmail.com>
2025-09-02 16:38:57 -0500

    Added start_time to indicate start of the candle

--- data-service/src/models/candles.py
+++ data-service/src/models/candles.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class Candle:
     symbol: str
     timeframe: str
+    start_time: datetime
     open: float
     high: float
     low: float
