8467c690877e9f298b8d41d0209b2c5cffa02ba8
mgordon34 <coltfanman@gmail.com>
2025-09-04 16:31:33 -0500

    Added markettype enums

--- data-service/src/common/enums.py
+++ data-service/src/common/enums.py
@@ -1,8 +1,19 @@
 from enum import StrEnum
 
 
+class MarketType(StrEnum):
+    FUTURES = "Futures"
+    EQUITIES = "Equities"
+
+
 class FuturesProduct(StrEnum):
     NQ = "NQ"
     ES = "ES"
     YM = "YM"
     GC = "GC"
+
+
+def get_market_type(symbol: str) -> MarketType:
+    if symbol in FuturesProduct:
+        return MarketType.FUTURES
+    return MarketType.EQUITIES
