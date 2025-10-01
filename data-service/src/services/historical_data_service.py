e021394c6f9c94794d25e8efb03801030987eaab
mgordon34 <coltfanman@gmail.com>
2025-10-01 12:47:46 -0500

    Updated data request

--- data-service/src/services/historical_data_service.py
+++ data-service/src/services/historical_data_service.py
@@ -1,6 +1,7 @@
 import os
 from dataclasses import dataclass
 from datetime import datetime, timedelta
+from typing import Optional
 
 import databento
 import pandas as pd
@@ -18,33 +19,33 @@ class HistoricalDataService:
     symbols: list[str]
     timeframe: str
 
+    def __post_init__(self):
+        self.curr_dbn_file: Optional[databento.DBNStore] = None
+
     def publish_historical_data(self) -> dict[str, dict[datetime, Candle]]:
         logger.info(f"get_historical_data[{self.symbols}]: {self.start_time} - {self.end_time}")
 
-        # data: dict = {s: {} for s in self.symbols}
-
-        for symbol in self.symbols:
-            df = self.get_db_data(symbol)
-            dbn_store = databento.DBNStore.from_file(path="data/glbx-mdp3-20250801-20250828.ohlcv-1m.dbn.zst")
-            # dbn_store = databento.DBNStore.from_file(path="data/glbx-mdp3-20200829-20250828.ohlcv-1m.dbn.zst")
-
-        df = dbn_store.to_df()
-        logger.info(f"Retrieved {len(df)} entries in data frame")
-
-        return self._deserialize_dbn(df)
-
-    def get_db_data(self, symbol: str) -> pd.DataFrame:
         curr_date: datetime.date = self.start_time.astimezone(pytz.utc).date()
         end_date: datetime.date = self.end_time.astimezone(pytz.utc).date()
-        dbns: list[str] = os.listdir("data")
-        logger.debug(f"data dir: {dbns}")
         while curr_date <= end_date:
             logger.debug(f"Getting db for {curr_date}")
 
+            dbn_store = self.get_dbn_for_date(curr_date)
+            df = dbn_store.to_df()
+            logger.info(f"Retrieved {len(df)} entries in data frame")
+            logger.info(self._deserialize_dbn(df))
+
             curr_date += timedelta(days=1)
 
-    def get_dbn_for_date(date: datetime.date) -> databento.DBNStore:
+        return "OK"
+
+    def get_dbn_for_date(self, date: datetime.date) -> databento.DBNStore:
         logger.debug(f"Finding dbn store for {date}")
+
+        for dbn_path in os.listdir("data"):
+            if _date_in_dbn_str(date, dbn_path):
+                logger.debug(f"{date} in {dbn_path}")
+                return databento.DBNStore.from_file(path="data/" + dbn_path)
 
     def _deserialize_dbn(self, df: pd.DataFrame) -> dict[str, dict[datetime, Candle]]:
         data: dict = {s: {} for s in self.symbols}
@@ -61,3 +62,15 @@ class HistoricalDataService:
                 )
 
         return data
+
+
+def _date_in_dbn_str(date: datetime.date, dbn_path: str) -> bool:
+    if "ohlc" not in dbn_path:
+        return False
+
+    dbn_words = dbn_path.split("/")[-1].split(".")[0].split("-")
+    date_format = "%Y%m%d"
+    start_date = datetime.strptime(dbn_words[2], date_format).date()
+    end_date = datetime.strptime(dbn_words[3], date_format).date()
+
+    return start_date <= date <= end_date
